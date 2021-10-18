f7e3d33bf724e347c3e67016a8d10e965eb09b46
alextexas <jolanotei@gmail.com>
2021-10-18 16:15:20 -0300

    .

--- DB/conexion.py
+++ DB/conexion.py
@@ -59,6 +59,7 @@ def crear_tabla():  # crea una tabla (al iniciar por primera vez el programa se 
     apellido VARCHAR(20) NOT NULL,
     dni VARCHAR(20) NOT NULL,
     mail VARCHAR(50) NOT NULL,
+    telefono VARCHAR(50) NOT NULL,
     domicilio VARCHAR(50) NOT NULL,
     foto VARCHAR(20) NOT NULL,
     nacimiento DATE NOT NULL,
