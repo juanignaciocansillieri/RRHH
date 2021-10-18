0e1cc056652b64dc1dfa4e3313b9b7aaef577364
juancansi <juancansi@gmail.comgit config --global user.name Your>
2021-10-18 14:20:04 -0300

    main

---
+++ main/main_func.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import PyQt5
+from PIL import Image
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+from PyQt5 import QtCore
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+sys.path.append("C:\\RRHH\\login\\")
+from login import Ui_MainWindow
+
+
+
+defaultImg = ""
+codigoViejo = ""
+DNI_Viejo = ""
+DNI = ""
+
+class Modern(QMainWindow):
+
+    def __init__(self):
+        super(Modern, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        ############# RECIBIMOS PROPORCIONES DE LA PANTALLA ###########
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
+        ############## CENTRAMOS LA VENTANA #############
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
+        self.move(qtRectangle.topLeft())
